38aa579ad03599f8174c986b33e850ea607b1127
Marcus Leong <mwleong25@gmail.com>
2019-10-26 15:30:45 -0500

    Prepare Screen

--- ui/src/components/PrepareScreen.js
+++ ui/src/components/PrepareScreen.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View, Button } from 'react-native';
+import { Header, SearchBar } from 'react-native-elements';
 
 class PrepareScreen extends Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
   render() {
+    const { search } = this.state;
     return(
-      <View>
-        <Text>
-          Prepare screen
-        </Text>
+      <View style={{marginTop:20}}>
+        <Header leftComponent={{ icon: 'whatshot' }}
+          statusBarProps={{ barStyle: 'light-content' }}
+          barStyle="light-content"
+          centerComponent={{ h3: 'Spice Up The Night', style: { color: '#fff' } } }
+          containerStyle={{
+          backgroundColor: '#3D6DCC',
+          justifyContent: 'space-around',
+          }}
+        />
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={this.updateSearch}
+          value={search}
+        />
+        <Button
+          title="Search"
+          color="red"
+			  />
       </View>
     );
   }
