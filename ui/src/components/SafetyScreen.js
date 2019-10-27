0a4369104caa3d326ffd8378212aabed1ff92c07
Nina Boonyasiti <nboonyasitin@gmail.com>
2019-10-26 23:08:30 -0500

    WIP on MapView

--- ui/src/components/SafetyScreen.js
+++ ui/src/components/SafetyScreen.js
@@ -1,25 +1,49 @@
 import React from "react";
-import { StyleSheet,Text, View } from "react-native";
+import { StyleSheet,Text, View, Dimensions } from "react-native";
 import GetLocation from './GetLocation';
-import { MapView, Permissions } from 'expo'
+import { Permissions } from 'expo'
+import MapView from 'react-native-maps';
 
 export default class SafetyScreen extends React.Component {
+  state = {
+    latitude: null,
+    longitude: null,
+  }
+
+  async DidMount() {
+    const { status } = await Permissions.getAsync(Permissions.LOCATION)
+
+    if(status !== 'granted') {
+      const response = await Permissions.askAsync(Permissions.LOCATION)
+    }
+  }
+
   getUserLocation = () => {
     navigator.geolocation.getCurrentPosition(position => {
       console.log(position);},
        err => console.log(err));
   }
-  state = {
-    latitude: null,
-    longitude: null,
-  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Keep your friends safe, make sure they get home.</Text>
         <Text>Let's get a location!</Text>
         <GetLocation onmyLocation={this.getUserLocation} /> 
+        <MapView style={{flex: 1 }}
+        initialRegion = {{
+          //latitude,
+          //longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }} >
+          </MapView>
       </View>
+      // <View style={styles.container}>
+      //   <Text>Keep your friends safe, make sure they get home.</Text>
+      //   <Text>Let's get a location!</Text>
+      //   <GetLocation onmyLocation={this.getUserLocation} /> 
+      // </View>
     );
   }
 }
@@ -31,4 +55,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
