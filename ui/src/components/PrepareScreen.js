203d3e79212643b042da9f8101ade30c0f80096c
Marcus Leong <mwleong25@gmail.com>
2019-10-26 21:55:17 -0500

    Search and voice UI

--- ui/src/components/PrepareScreen.js
+++ ui/src/components/PrepareScreen.js
@@ -1,120 +1,130 @@
 import React, { Component } from 'react';
 import { View, Button } from 'react-native';
-import { Header, SearchBar } from 'react-native-elements';
-import {
-  Stitch,
-  RemoteMongoClient,
-  BSON
-} from "mongodb-stitch-browser-sdk";
-
-
-const items = mongodb.db("MyData").collection("ulta_product");
-const product_avail = mongodb.db("MyData").collection("ulta");
-const stores = mongodb.db("MyData").collection("ulta_store");
+import { Header, SearchBar, Icon, Input } from 'react-native-elements';
 
 class PrepareScreen extends Component {
+  state = {
+    search: '',
+  };
 
-  onstructor() {
-    super();
-    this.state = {
-      value: "",
-      result: [],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.displayItems = this.displayItems.bind(this);
-    this.addItem = this.addItem.bind(this);
-  }
+  updateSearch = search => {
+    this.setState({ search });
+  };
   
-  componentDidMount() {
-    // Initialize the App Client
-    this.client = Stitch.initializeDefaultAppClient("hopper-ylufi");
-    // Get a MongoDB Service Client
-    // This is used for logging in and communicating with Stitch
-    const mongodb = this.client.getServiceClient(
-      RemoteMongoClient.factory,
-      "mongodb-atlas"
-    );
-    // Get a reference to the todo database
-    this.db = mongodb.db("MyData");
-    this.displayItemsOnLoad();
+  startRecording = async () => {
+    const {
+      status
+    } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    if (status !== 'granted') return;
+
+    this.setState({
+      isRecording: true
+    });
+    // some of these are not applicable, but are required
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: true,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      playThroughEarpieceAndroid: true,
+
+    });
+    const recording = new Audio.Recording();
+    try {
+      await recording.prepareToRecordAsync(recordingOptions);
+      await recording.startAsync();
+    } catch (error) {
+      console.log(error);
+      this.stopRecording();
+    }
+    this.recording = recording;
   }
-  
-  displayItems() {
-    // query the remote DB and update the component state
-    this.db
-      .collection("ulta_product")
-      .find({}, { limit: 100 })
-      .asArray()
-      .then(result => {
-        this.setState({result});
+
+  getTranscription = async () => {
+    this.setState({
+      isFetching: true
+    });
+    try {
+      const info = await FileSystem.getInfoAsync(this.recording.getURI());
+      console.log(`FILE INFO: ${JSON.stringify(info)}`);
+      const uri = info.uri;
+      const formData = new FormData();
+      formData.append('file', {
+        uri,
+        type: 'audio/x-wav',
+        // could be anything 
+        name: 'speech2text'
       });
-   }
-  displayItemsOnLoad() {
-    // Anonymously log in and display comments on load
-    this.client.auth
-      .loginWithCredential(new AnonymousCredential())
-      .then(this.displayItems)
-      .catch(console.error);
-  }
-  findItem(event) {
-    event.preventDefault();
-    const { value } = this.state;
-    // insert the todo into the remote Stitch DB
-    // then re-query the DB and display the new todos
-
-    const query = { "DESCRIPTION": { "$regex": ".* "+value + " .*" } };
-    const options = {
-      "projection": { "_id": 0 },
-      "sort": { "DISPLAY_NAME": 1 }
-    };
-
-    this.db
-      .collection("ulta_product")
-      .find(query, options)
-      .asArray()
-      .then(result => {
-        this.setState({result});
+      const response = await fetch(config.CLOUD_FUNCTION_URL, {
+        method: 'POST',
+        body: formData
       });
+      const data = await response.json();
+      this.setState({
+        query: data.transcript
+      });
+    } catch (error) {
+      console.log('There was an error', error);
+      this.stopRecording();
+      this.resetRecording();
+    }
+    this.setState({
+      isFetching: false
+    });
   }
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  
 
   render() {
+    const { search } = this.state;
     return(
-      <View style={{marginTop:20}}>
-        <Header leftComponent={{ icon: 'whatshot' }}
+      <View style={{marginTop:16}}>
+        <Header 
+          leftComponent={<Icon
+                          name='whatshot'
+                          color='red'
+                          size={35}
+                        />}
+          placement="center"
           statusBarProps={{ barStyle: 'light-content' }}
           barStyle="light-content"
-          centerComponent={{ h3: 'Spice Up The Night', style: { color: '#fff' } } }
+          centerComponent={{ text: 'Spice Up The Night', style: { fontSize: 22, fontWeight: 'bold', color: '#fff' } } }
           containerStyle={{
-          backgroundColor: '#3D6DCC',
-          justifyContent: 'space-around',
+            backgroundColor: '#3D6DCC',
+            justifyContent: 'space-around',
           }}
         />
-        <form onSubmit={this.addItem}>
-          <SearchBar 
-            placeholder="Type Here..."
-            onChangeText={this.handleChange}
-            value={this.state.value}
+        <View style={{marginTop:10, flexDirection:"row"}}>
+          <Input 
+            placeholder="Search..."
+            containerStyle={{width:300}}
+            // rightIcon={
+            //   <Icon
+            //     raised
+            //     size={17}
+            //     name="search"
+            //   />
+            // }
           />
-          <input type="submit" value="Submit" 
-            title="Search"
-            color="red"
+          <Icon
+            raised
+            justifyContent="flex-end"
+            size={18}
+            name="search"
           />
-        </form>
-        <ul>
-          {/* Map over the todos from our remote DB */}
-          {this.state.result.map(res => {
-            return <li>{res.item}</li>;
-          })}
-        </ul>
+        </View>
+        
+        <Icon
+          raised
+          placement="right"
+          name="keyboard-voice"
+          type='material-icons'
+          color="black"
+          size={30}
+          containerStyle={{marginLeft: 270, marginTop: 350}}
+        />
       </View>
     );
   }
-
 }
 
 export default PrepareScreen;
